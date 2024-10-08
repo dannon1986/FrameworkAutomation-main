915643dcc32b1727c39e6047977e74a82853c08a
dannon1986 <dannon1986@gmail.com>
2024-10-08 13:24:33 +0300

    Commit number 1

--- defaultFrameworkAutomation-main/src/test/java/test/authorization/Authorization.java
+++ defaultFrameworkAutomation-main/src/test/java/test/authorization/Authorization.java
@@ -2,27 +2,30 @@ package test.authorization;
 
 import org.testng.annotations.Test;
 import pages.authorization.AuthorizationPage;
-import pages.constructor.ConstructorPage;
 import services.authorization.AuthorizationService;
 
-import static com.codeborne.selenide.Condition.visible;
 
+// Создаём публичный клаас Авторизация (Authorization)
 public class Authorization {
 
+// Создаём два новых неизменяемых объекта импортированных классов
   private final AuthorizationService authorizationService = new AuthorizationService();
   private final AuthorizationPage authorizationPage = new AuthorizationPage();
-  private final ConstructorPage constructorPage = new ConstructorPage();
 
+
+//Создаём метод для проверки регистрации с заданными параметрами емейла и пароля.
   @Test
   public void checkAuthorization() {
     String email = "alex20-03sh@mail.ru";
     String password = "022093Aa";
 
+    // Используем в данном методе методы классов authorizationService и authorizationPage,
+    // которые входят на страницу авторизации, вводят емейл с паролем и проверяют все возможные действия.
+
     authorizationService.openAutorizationPage();
     authorizationPage.setEmail(email);
     authorizationPage.setPassword(password);
     authorizationPage.getEnterButton().click();
-    constructorPage.getTitle()
-        .shouldBe(visible.because("Пользователь не авторизован"));
+
   }
 }
